88592049819d2913c5f329be185b16a826a6bc03
Student <milena-vnukova@mail.ru>
2021-10-22 11:08:32 +0300

    Conflict resolved

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -3,8 +3,9 @@ using namespace std;
 int main() {
 	char name[20];
 	cout << "Enter your name: ";
+	std::cout << "Hello world! Have a nice day!\n"
 	cin >> name;
-	cout << "Hello, have a nice day, " << name << "!\n";
+	cout << "Hello, " << name << "!\n";
 	cout << "Nice to see you learning Git\n";
 	system("pause");
 	return 0;
