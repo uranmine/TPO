30774e0dcb5436ac5b602046252940c67e47c0e9
Student <milena-vnukova@mail.ru>
2021-10-22 11:04:00 +0300

    "added having a nice day wish"

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -1,6 +1,11 @@
 ﻿#include <iostream>
+using namespace std;
 int main() {
-	std::cout << "Hello world!\n";
+	char name[20];
+	cout << "Enter your name: ";
+	cin >> name;
+	cout << "Hello, have a nice day, " << name << "!\n";
+	cout << "Nice to see you learning Git\n";
 	system("pause");
 	return 0;
 }
