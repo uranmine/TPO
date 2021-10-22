16b796f8d595eafb977a676058c7865c84280fdb
Student <milena-vnukova@mail.ru>
2021-10-22 10:59:14 +0300

    "added student name input"

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -1,6 +1,11 @@
-﻿#include <iostream>
+﻿
+#include <iostream>
+using namespace std;
 int main() {
-	std::cout << "Hello world!\n";
+	char name[20];
+	cout << "Enter your name: ";
+	cin >> name;
+	cout << "Hello, " << name << "!\n";
 	system("pause");
 	return 0;
 }
